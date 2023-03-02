91b11ed8031599e53adab0970e135bf4edfd7594
DavidCundumi <89714703+DavidCundumi@users.noreply.github.com>
2023-03-02 16:57:18 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  console.log("Peticion recibida");
+  res.send('Hola mundo')
+});
+
+app.listen(4000,()=>{
+console.log("Servidor escuchando en el puerto 4000");
+})
