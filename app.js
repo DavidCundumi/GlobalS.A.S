5dcc785ec03986a2fb1580c6c4e6ed8aa7090582
DavidCundumi <89714703+DavidCundumi@users.noreply.github.com>
2023-03-02 18:19:27 -0500

    Prueba

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ const express = require("express");
 const path = require("path");
 require("dotenv").config();
 const app = express();
-app.get("/", (req, res) => {
-  console.log("Peticion recibida");
-  res.sendFile("../dist/global-s.a.s/index.html", { root: __dirname });
-});
+
 app.use(express.static(path.join(__dirname, "public")));
 const port = process.env.PORT;
 
