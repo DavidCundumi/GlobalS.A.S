7695abadf3d58e3560fa966760756b3b060329fa
DavidCundumi <89714703+DavidCundumi@users.noreply.github.com>
2023-03-02 22:29:55 -0500

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 const port = process.env.PORT;
