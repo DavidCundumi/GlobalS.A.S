ea3c54b2e49026d711e6d8f5661061d232cda42e
DavidCundumi <89714703+DavidCundumi@users.noreply.github.com>
2023-03-02 22:13:44 -0500

    Pimer commit

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-app.get("/", (req, res) => {
-  console.log("Peticion recibida");
-  res.send("Hola mundo");
-});
-//app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 const port = process.env.PORT;
 
 app.listen(port, () => {
